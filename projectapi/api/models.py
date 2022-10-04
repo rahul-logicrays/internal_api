1c441aeae4cdcbd6c0909cec4abac931c6f2e82a
rahul-logicrays <rahul.d@logicrays.com>
2022-10-04 11:43:00 +0530

    Created model and apply migrations

--- projectapi/api/models.py
+++ projectapi/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class SalesHistory(models.Model):
+    saleshistory_id = models.BigAutoField(primary_key=True)
+    sku = models.CharField(max_length=50)
+    tenate_id = models.CharField(max_length=55)
+    node = models.CharField(max_length=50)
+    channle = models.CharField(max_length=50)
+    salesman_email = models.CharField(max_length=50)
+    salesman_name = models.CharField(max_length=50)
+    actual_sales_volume = models.IntegerField()
+    actual_sales_volume_uom = models.CharField(max_length=50)
+    actual_sales_value = models.IntegerField()
+    actual_sales_value_uom = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.saleshistory_id
