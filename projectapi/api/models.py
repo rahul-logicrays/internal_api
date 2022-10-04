ad5f75c76a4de0f9c888fc58adcbe5966c4f4d3a
HardikLogicrays <hardiksathavara@logicrays.com>
2022-10-04 15:46:23 +0530

    serching and ordering functionality.

--- projectapi/api/models.py
+++ projectapi/api/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
 class SalesHistory(models.Model):
     saleshistory_id = models.BigAutoField(primary_key=True)
-    sku = models.CharField(max_length=50)
-    tenate_id = models.CharField(max_length=55)
-    node = models.CharField(max_length=50)
-    channle = models.CharField(max_length=50)
-    salesman_email = models.CharField(max_length=50)
-    salesman_name = models.CharField(max_length=50)
-    actual_sales_volume = models.IntegerField()
-    actual_sales_volume_uom = models.CharField(max_length=50)
-    actual_sales_value = models.IntegerField()
-    actual_sales_value_uom = models.CharField(max_length=50)
+    sku = models.CharField(max_length=50, null=True)
+    tenate_id = models.CharField(max_length=55, null=True)
+    node = models.CharField(max_length=50, null=True)
+    channle = models.CharField(max_length=50, null=True)
+    salesman_email = models.CharField(max_length=50, null=True)
+    salesman_name = models.CharField(max_length=50, null=True)
+    actual_sales_volume = models.IntegerField(null=True)
+    actual_sales_volume_uom = models.CharField(max_length=50, null=True)
+    actual_sales_value = models.IntegerField(null=True)
+    actual_sales_value_uom = models.CharField(max_length=50, null=True)
 
-    def __str__(self) -> str:
-        return self.saleshistory_id
+    def __str__(self):
+        return str(self.saleshistory_id)
--- projectapi/api/views.py
+++ projectapi/api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import SalesHistory
+from .serlializers import GetSalesHistorySerializer
+from .paginations import CustomPagination
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .filters import CustomSearchFilter
 
-# Create your views here.
+
+class GetSalesHistoryView(ListAPIView):
+
+    queryset = SalesHistory.objects.all()
+    serializer_class = GetSalesHistorySerializer
+    pagination_class = CustomPagination
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['tenate_id']
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        response.data = {'status': status.HTTP_200_OK,
+                         'data': response.data, 'msg': 'Data Retrived.'}
+        return response
